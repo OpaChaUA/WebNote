c518183a1db68e769787e8cc6acbe751e0587042
Yevhenii Makovskyi <makofujin@gmail.com>
2023-07-26 01:36:18 +0300

    Initial commit

---
+++ src/main/java/webnotes/JavaDevTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package webnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevTeamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevTeamProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webnotes/JavaDevTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package webnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevTeamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
