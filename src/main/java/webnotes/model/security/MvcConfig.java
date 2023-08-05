b7f1f4a8f7f5af9a726b840e35d8a7147ab61fbc
AcceleratorMX <redfield242@gmail.com>
2023-08-05 18:47:35 +0300

    1

---
+++ src/main/java/webnotes/model/security/MvcConfig.java
@@ -0,0 +1,18 @@
+package webnotes.model.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/css/**")
+                .addResourceLocations("classpath:/static/css/");
+        registry
+                .addResourceHandler("/static/js/**")
+                .addResourceLocations("classpath:/static/js/");
+    }
+}
